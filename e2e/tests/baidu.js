32fd474fee8327099188a33c3ac8c1d8d598df70
Jiang yanzhu <wb-jiangyanzhu@hz.ali.com>
2017-03-29 15:13:36 +0800

    '换一种方式设计测试用例'

--- e2e/tests/baidu.js
+++ e2e/tests/baidu.js
@@ -1,16 +1,21 @@
 module.exports = {
   tags: ['测试baidu'],
-  'Test Baidu' : function(client){
+  'Step One' : function(client){
     client
       .url('http://www.baidu.com')
+      .waitForElementVisible('body',1000)
       .assert.title('百度一下，你就知道')
       .assert.visible('input[id=kw]')
       .assert.visible('input[id=su]')
       .setValue('input[id=kw]','nightwatch')
-      .click('input[id=su]');
-
+      .waitForElementVisible('input[id=su]',1000)
       client.pause(5000);
+  },
 
+  'Step Two' : function(client){
+    client
+      .click('input[id=su]');
+      client.pause(5000);
       client.end();
   }
 }
